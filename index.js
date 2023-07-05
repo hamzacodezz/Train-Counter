40eb6d5ca6e8e8cc7bf422d48a63f3ef9833fe66
Hamza <102821708+hamzacodezz@users.noreply.github.com>
2023-07-05 02:00:41 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,17 @@
+// document.getElementById("count-el").innerText = 5;
+
+let count = 0
+let countEl = document.getElementById("count-el")
+let saveEl = document.getElementById("save-el")
+function increment() {
+    count++
+    countEl.textContent = count
+}
+
+function save() {
+    let savedCount = count + " - "
+    saveEl.textContent += savedCount
+    count = 0
+    countEl.textContent = count
+}
+
